e5c5c6b7fb272e8c67682dfef638e714bf7c5548
Johan Lyheden <Johan.Lyheden@UNIBET.com>
2015-07-22 18:21:37 +0200

    added more test cases

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
+
 describe 'influxdb' do
 
   context 'with defaults for all parameters' do
+    it { should compile.with_all_deps }
     it { should contain_class('influxdb') }
     it { should contain_user('influxdb') }
     it { should contain_group('influxdb') }
@@ -9,9 +11,11 @@ describe 'influxdb' do
     it { should contain_service('influxdb') }
     it { should contain_file('/etc/default/influxdb').with_content(/USER=influxdb\nGROUP=influxdb\nSTDERR=/) }
     it { should contain_file('/etc/opt/influxdb/influxdb.conf').with_content(/\[retention\]\n  check-interval = \"10m\"\n  enabled = true\n/) }
+    it { should contain_class('influxdb::package').that_notifies('Class[influxdb::service]') }
+    it { should contain_class('influxdb::config').that_notifies('Class[influxdb::service]') }
   end
 
-  context 'with custom settings' do
+  context 'with any configuration setting (config_retention here) custom setting' do
     let :params do {
       :config_retention => {
         'foo'    => 'bar',
@@ -19,7 +23,25 @@ describe 'influxdb' do
       }
     } end
 
-    it { should contain_file('/etc/opt/influxdb/influxdb.conf').with_content(/\[retention\]\n  foo = \"bar\"\n  number = 10\n/)}
+    it { should contain_file('/etc/opt/influxdb/influxdb.conf').with_content(/\n\[retention\]\n  foo = \"bar\"\n  number = 10\n\n/)}
+  end
+
+  context 'with service_refresh => false' do
+    let :params do {
+      :service_refresh => false
+    } end
+
+    it { should compile.with_all_deps }
+    it { should_not contain_class('influxdb::package').that_notifies('Class[influxdb::service]') }
+    it { should_not contain_class('influxdb::config').that_notifies('Class[influxdb::service]') }
+  end
+
+  context 'with any configuration setting disabled' do
+    let :params do {
+      :config_retention => false
+    } end
+
+    it { should_not contain_file('/etc/opt/influxdb/influxdb.conf').with_content(/\n\[retention\]\n/) }
   end
 
 end
