6494313658ad9133dc76e4c778c0c0c5917d99e0
Johan Lyheden <Johan.Lyheden@UNIBET.com>
2015-08-08 21:44:40 +0200

    added database management

---
+++ manifests/management/database.pp
@@ -0,0 +1,43 @@
+# == Define: influxdb::management::database
+#
+# Creates or drops an InfluxDB database
+#
+# === Parameters:
+#
+# [*namevar*]
+#   Name of database to manage
+#
+# [*ensure*]
+#   Ensurable, present - creates, absent - drops
+#
+define influxdb::management::database ( $ensure = 'present' ) {
+
+    $db_name = $name
+
+    validate_re($db_name, '^[a-zA-Z0-9]+$')
+
+    case $ensure {
+      present: {
+        $command = "influx -execute 'CREATE DATABASE ${db_name}''"
+        $unless = "influx -execute 'SHOW DATABASES' | grep -qs \"^${db_name}\$\""
+        $onlyif = undefined
+      }
+      absent: {
+        $command = "influx -execute 'DROP DATABASE ${db_name}'"
+        $unless = undefied
+        $onlyif = "influx -execute 'SHOW DATABASES' | grep -qs \"^${db_name}\$\""
+      }
+      default: {
+        fail("Unsupported :ensure value ${ensure}")
+      }
+    }
+
+    exec { "manage_influxdb_${db_name}":
+      path    => '/opt/influxdb',
+      command => $command,
+      unless  => $unless,
+      onlyif  => $onlyif,
+      require => Class['::influxdb::service']
+    }
+
+}
